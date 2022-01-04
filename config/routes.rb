f4185557336e844858ed8a282016945e9b549a79
kirby_lam <kirbylam16@gmail.com>
2022-01-03 21:55:20 -0800

    Added simple RESTful actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'kittens#index'
+
+  resources :kittens, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
